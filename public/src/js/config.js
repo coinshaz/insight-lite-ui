1f57b6417e26233c3c9235a61ef7b44cf4c1414e
coinshaz <coinshazar@gmail.com>
2017-10-07 17:56:09 +0300

    Change for unitedcore

--- public/src/js/config.js
+++ public/src/js/config.js
@@ -5,7 +5,7 @@ angular.module('insight').config(function($routeProvider) {
   $routeProvider.
     when('/block/:blockHash', {
       templateUrl: 'views/block.html',
-      title: 'Litecoin Block '
+      title: 'Unitedcoin Block '
     }).
     when('/block-index/:blockHeight', {
       controller: 'BlocksController',
@@ -17,7 +17,7 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/tx/:txId/:v_type?/:v_index?', {
       templateUrl: 'views/transaction.html',
-      title: 'Litecoin Transaction '
+      title: 'Unitedcoin Transaction '
     }).
     when('/', {
       templateUrl: 'views/index.html',
@@ -25,15 +25,15 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/blocks', {
       templateUrl: 'views/block_list.html',
-      title: 'Litecoin Blocks solved Today'
+      title: 'Unitedcoin Blocks solved Today'
     }).
     when('/blocks-date/:blockDate/:startTimestamp?', {
       templateUrl: 'views/block_list.html',
-      title: 'Litecoin Blocks solved '
+      title: 'Unitedcoin Blocks solved '
     }).
     when('/address/:addrStr', {
       templateUrl: 'views/address.html',
-      title: 'Litecoin Address '
+      title: 'Unitedcoin Address '
     }).
     when('/status', {
       templateUrl: 'views/status.html',
